a1e193fad1037ed98869d5a0def7f546a32d3a88
Veranika Vasilchykava <veranika.v@fundamentalvr.com>
2024-12-02 17:47:19 +0200

    add three tasks

---
+++ capitalize-the-first-letter-of-each-word.js
@@ -0,0 +1,23 @@
+/**
+    Capitalize the First Letter of Each Word.
+
+    Description:
+    Write a function that capitalizes the first letter of each word in a string.
+
+    Requirements:
+    Create a function that takes a string as input,
+    returns the string with the first letter of each word capitalized.
+    
+    Example Input: capitalizeWords('hello world')
+    Example Output: 'Hello World'
+    
+    Bonus Points:
+        Handle cases with extra spaces or punctuation.
+        Ignore non-alphabetic characters.
+ */
+
+function capitalizeWords(string) {
+
+}
+
+capitalizeWords('hello world');
